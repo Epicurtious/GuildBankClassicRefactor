55e640e2cc030befaf3ae878a8e0928178d3dda5
cgoodwin117 <curtisgoodwin117@yahoo.com>
2024-06-05 14:52:34 -0600

    Added option to get items from mail box, updated readme and version number

--- GuildBankClassicRefactor.lua
+++ GuildBankClassicRefactor.lua
@@ -4,19 +4,147 @@ GBC_LOADED = 0;
 local NUM_INVENTORY_BACKPACK_SPACES = 16;
 local NUM_MAX_BAG_SPACES = 18;
 local NUM_BANK_SPACES = NUM_BANKGENERIC_SLOTS;
+local NUM_MAX_MAIL_SLOTS = 100;
+local NUM_MAX_MAIL_CAPACITY = 12
 
 
-GBC_UNLOADER = CreateFrame("FRAME", "GBC_UNLOADER");
-GBC_UNLOADER:RegisterEvent("BANKFRAME_CLOSED");
-GBC_UNLOADER:SetScript("OnEvent", function() 
+GBC_UNLOADER_BANK = CreateFrame("FRAME", "GBC_UNLOADER_BANK");
+GBC_UNLOADER_BANK:RegisterEvent("BANKFRAME_CLOSED");
+GBC_UNLOADER_BANK:SetScript("OnEvent", function() 
 	GBC_LOADED = 0; 
 	GuildBankClassic:Hide(); 
 	GuildBankClassicButton1:Hide();
 end);
 
-GBC_LOADER = CreateFrame("FRAME", "GBC_LOADER");
-GBC_LOADER:RegisterEvent("BANKFRAME_OPENED");
-GBC_LOADER:SetScript("OnEvent", function()
+GBC_UNLOADER_MAIL = CreateFrame("FRAME", "GBC_UNLOADER_MAIL");
+GBC_UNLOADER_MAIL:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_HIDE");
+GBC_UNLOADER_MAIL:SetScript("OnEvent", function(self, event, eventType, ...)
+	if event == "PLAYER_INTERACTION_MANAGER_FRAME_HIDE" and eventType == Enum.PlayerInteractionType.MailInfo then
+		GBC_LOADED = 0; 
+		GuildBankClassic:Hide(); 
+		GuildBankClassicButton2:Hide();
+	end
+end);
+
+GBC_LOADER_MAIL = CreateFrame("FRAME", "GBC_LOADER_MAIL");
+GBC_LOADER_MAIL:RegisterEvent("MAIL_SHOW");
+GBC_LOADER_MAIL:SetScript("OnEvent", function()
+	GBC_LOADED = 1; 
+	GuildBankClassicButton2 = CreateFrame("BUTTON", "Button", CENTER, "UIPanelButtonTemplate");
+	GuildBankClassicButton2:SetWidth(45);
+	GuildBankClassicButton2:SetHeight(24);
+	GuildBankClassicButton2:SetPoint("CENTER", UIParent, "BOTTOM", 0, 150);
+	GuildBankClassicButton2:SetText("GBC");
+	GuildBankClassicButton2:SetMovable(true);
+	GuildBankClassicButton2:EnableMouse(true);
+	GuildBankClassicButton2:RegisterForDrag("LeftButton");
+	GuildBankClassicButton2:SetScript("OnDragStart", GuildBankClassicButton2.StartMoving);
+	GuildBankClassicButton2:SetScript("OnDragStop", GuildBankClassicButton2.StopMovingOrSizing);
+	GuildBankClassicButton2:SetScript("OnClick", function()
+		local GBC_Editbox_Text_Mail = "";
+		
+		if GBC_LOADED == 0 then
+			print("ERROR");
+			GuildBankClassic.EditBox:SetText("ERROR");
+		else
+			-- each entry in the array is in the form: {ID, Count}
+			local GBC_Mail_Items = {}
+			local GBC_Mail_Money = 0
+			local mail_inv_index = 1
+			for i = 1, NUM_MAX_MAIL_SLOTS do
+				if i <= GetInboxNumItems() then
+					local _, _, sender, subject, money, codMoney, _, hasItem = GetInboxHeaderInfo(i)
+					GBC_Mail_Money = GBC_Mail_Money + money
+					if hasItem and codMoney == 0 then
+						for j = 1, NUM_MAX_MAIL_CAPACITY do
+							local name, itemID, texture, count, quality, canUse = GetInboxItem(i, j)
+							if name then
+								GBC_Mail_Items[mail_inv_index] = {itemID, count};
+								mail_inv_index = mail_inv_index + 1
+							else
+								GBC_Mail_Items[mail_inv_index] = {"", ""};
+								mail_inv_index = mail_inv_index + 1
+							end
+						end
+					else
+						for j = 1, NUM_MAX_MAIL_CAPACITY do
+							GBC_Mail_Items[mail_inv_index] = {"", ""};
+							mail_inv_index = mail_inv_index + 1
+						end
+					end
+				else
+					for j = 1, NUM_MAX_MAIL_CAPACITY do
+						GBC_Mail_Items[mail_inv_index] = {"", ""};
+						mail_inv_index = mail_inv_index + 1
+					end
+				end
+			end
+			GBC_Money_Data_Mail = (("%dg %ds %dc"):format(GBC_Mail_Money / 100 / 100, (GBC_Mail_Money / 100) % 100, GBC_Mail_Money % 100));
+			-- add all the mail data to string for editbox
+			for i=1,#GBC_Mail_Items do
+				GBC_Editbox_Text_Mail = GBC_Editbox_Text_Mail .. GBC_Mail_Items[i][1] .. "\t" .. GBC_Mail_Items[i][2] .. "\n";
+			end
+			GBC_Editbox_Text_Mail = GBC_Editbox_Text_Mail .. date().."\n".. "\t" .. GBC_Money_Data_Mail .. "\n"
+			print("1")
+			GuildBankClassic.EditBox:SetText(GBC_Editbox_Text_Mail);
+			print("2")
+		end
+		GuildBankClassic:Show(); 
+		GuildBankClassic.EditBox:HighlightText();
+		
+		GuildBankClassic.Comment = CreateFrame("EditBox", "GuildBankClassic_Comment", GuildBankClassic);
+		GuildBankClassic.Comment:SetWidth(100);
+		GuildBankClassic.Comment:SetHeight(24);
+		GuildBankClassic.Comment:SetPoint("RIGHT", GuildBankClassicButton2, "RIGHT", -35, 0);
+		GuildBankClassic.Comment:SetFontObject("GameTooltipTextSmall");
+		GuildBankClassic.Comment:SetText("CTRL+C to copy");
+		GuildBankClassic.Comment:SetAutoFocus(false);
+		GuildBankClassic.Comment:SetMultiLine(false);
+		GuildBankClassic.Comment:EnableMouse(false);
+		
+
+	end);
+
+	GuildBankClassic = CreateFrame("Frame", "GuildBankClassic", UIParent, "BackdropTemplate");
+	GuildBankClassic:SetFrameStrata("HIGH");
+	GuildBankClassic:SetPoint("RIGHT", GuildBankClassicButton2, "RIGHT", 0, -42);
+	GuildBankClassic:SetWidth(150);
+	GuildBankClassic:SetHeight(60);
+	GuildBankClassic:SetBackdrop({
+		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background", 
+		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border", 
+		tile = true, tileSize = 16, edgeSize = 16, 
+		insets = { left = 4, right = 4, top = 4, bottom = 4 }
+	});
+	GuildBankClassic:SetMovable(false);
+	GuildBankClassic:SetClampedToScreen(true);
+	GuildBankClassic:SetResizable(false);
+	GuildBankClassic:EnableMouse(true);
+	GuildBankClassic:RegisterForDrag("LeftButton");
+	GuildBankClassic:SetScript("OnUpdate", function()
+		GuildBankClassic.EditBox:SetWidth(GuildBankClassic:GetWidth() - 13);
+		GuildBankClassic.EditBox:SetHeight(GuildBankClassic:GetHeight() - 13);
+		GuildBankClassic.ScrollFrame:UpdateScrollChildRect();
+	end);
+	GuildBankClassic:Hide();
+	GuildBankClassic.EditBox = CreateFrame("EditBox", "GuildBankClassic_EditBox", GuildBankClassic);
+	GuildBankClassic.EditBox:SetAutoFocus(true);
+	GuildBankClassic.EditBox:SetMultiLine(true);
+	GuildBankClassic.EditBox:EnableMouse(true);
+	GuildBankClassic.EditBox:SetPoint("CENTER", GuildBankClassic, "CENTER");
+	GuildBankClassic.EditBox:SetFontObject("GameTooltipTextSmall");
+	GuildBankClassic.EditBox:SetScript("OnEscapePressed", function() GuildBankClassic:Hide() end);
+	
+	GuildBankClassic.ScrollFrame = CreateFrame("ScrollFrame", "GuildBankClassic_ScrollFrame", GuildBankClassic, "UIPanelScrollFrameTemplate");
+	GuildBankClassic.ScrollFrame:SetPoint("TOPLEFT", GuildBankClassic, "TOPLEFT", 6, -6);
+	GuildBankClassic.ScrollFrame:SetPoint("BOTTOMRIGHT", GuildBankClassic, "BOTTOMRIGHT", -6, 6);
+	GuildBankClassic.ScrollFrame:SetScrollChild(GuildBankClassic.EditBox);
+end);
+
+
+GBC_LOADER_BANK = CreateFrame("FRAME", "GBC_LOADER_BANK");
+GBC_LOADER_BANK:RegisterEvent("BANKFRAME_OPENED");
+GBC_LOADER_BANK:SetScript("OnEvent", function()
 	GBC_LOADED = 1; 
 	GuildBankClassicButton1 = CreateFrame("BUTTON", "Button", CENTER, "UIPanelButtonTemplate");
 	GuildBankClassicButton1:SetWidth(45);
@@ -28,8 +156,7 @@ GBC_LOADER:SetScript("OnEvent", function()
 	GuildBankClassicButton1:RegisterForDrag("LeftButton");
 	GuildBankClassicButton1:SetScript("OnDragStart", GuildBankClassicButton1.StartMoving);
 	GuildBankClassicButton1:SetScript("OnDragStop", GuildBankClassicButton1.StopMovingOrSizing);
-	GuildBankClassicButton1:SetScript("OnClick", function() 
-
+	GuildBankClassicButton1:SetScript("OnClick", function()
 		local GBC_Editbox_Text = "";
 
 		if GBC_LOADED == 0 then
@@ -96,20 +223,32 @@ GBC_LOADER:SetScript("OnEvent", function()
 			-- end
 
 			local GBC_Bags_Info = {}
-			for i = 1, NUM_BAG_SLOTS + NUM_BANKBAGSLOTS do
+			local bag_info_index = 1
+			for i = NUM_BAG_SLOTS+1, NUM_BAG_SLOTS + NUM_BANKBAGSLOTS do
 				local invID = C_Container.ContainerIDToInventoryID(i);
 				local bagID = GetInventoryItemID("player", invID);
 				if bagID ~= nil then
-					GBC_Bags_Info[i] = bagID;
+					GBC_Bags_Info[bag_info_index] = bagID;
 				else
-					GBC_Bags_Info[i] = "";
+					GBC_Bags_Info[bag_info_index] = "";
 				end
+				bag_info_index = bag_info_index + 1
+			end
+			for i = 1, NUM_BAG_SLOTS do
+				local invID = C_Container.ContainerIDToInventoryID(i);
+				local bagID = GetInventoryItemID("player", invID);
+				if bagID ~= nil then
+					GBC_Bags_Info[bag_info_index] = bagID;
+				else
+					GBC_Bags_Info[bag_info_index] = "";
+				end
+				bag_info_index = bag_info_index + 1
 			end
 
 			-- debugging for bag item ids
-			-- for i=1,#GBC_Bags_Info do
-			-- 	print(i .. " -> " .. GBC_Bags_Info[i])
-			-- end
+			for i=1,#GBC_Bags_Info do
+				print(i .. " -> " .. GBC_Bags_Info[i])
+			end
 
 			GBC_money = GetMoney();
 			GBC_Money_Data = (("%dg %ds %dc"):format(GBC_money / 100 / 100, (GBC_money / 100) % 100, GBC_money % 100));
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # GuildBankClassicRefactor
+
 This is a refactor and update of the addon GuildBankClassic that is used in conjunction with Google Sheets to show the contents of a bank alt for a guild.
 
 # Known Bugs/Issues
+
 If you setup authentication with the Blizzard app, they will give you an extra 4 bag spaces in your backpack. The way the addon was written to work with google sheets is very rigid and would take a bit more effort on both the addon side and the google sheets side to add those extra 4 bag spaces. You may notice the bottom 4 items in your bag are missing from the guild bank if you have those extra slots, this is expected. I do not intend to fix this at the time, just know you will have to leave those slots empty for this to work.
 
 If there ever comes a day where there are bags bigger then 18 slots in Classic, this addon and the google sheets page will need some work. As stated in the previous issue, the addon is very rigid in the way it was written and I do not intent to fix the design issues of the addon AND the Google Sheets document.
 
 # Instructions
+
 1. Download GuildBankClassic AddOn and put it to your World of Warcraft's Interface\AddOns folder
 
-2. Make a copy of this spreadsheet to your Google Drive ([Link](https://docs.google.com/spreadsheets/d/1ZzkpWI0wNriGf4btzAdqnqEMhrZQnKvgVm5Eo7zxwE0/edit?pli=1#gid=1895612339))
+2. Make a copy of this spreadsheet to your Google Drive ([Link](https://docs.google.com/spreadsheets/d/1RLl5qZC5h1e_H18x5xLeOBJZ5j28pLjsD4RPTMd1jNM/edit#gid=1895612339))
 
 ![Google Sheets Example Image](https://i.imgur.com/iDMcicS.png)
 
@@ -33,11 +36,7 @@ If there ever comes a day where there are bags bigger then 18 slots in Classic, 
 
 10. You are ready to go! Now you can share your GuildBankClassic
 
- 
-
 You can put your share link to in-game guild's information tab or to guild's Discord chat so that any member could easily access it
-
- 
 
 [Google FAQ: Make a copy of a file](https://support.google.com/docs/answer/49114/)
 
